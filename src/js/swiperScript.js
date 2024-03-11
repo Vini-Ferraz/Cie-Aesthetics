7acd8010f3c368307ba21bcc16dd472d1968c16a
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-03-11 19:18:20 -0300

    Adicionando novas imagens
    Adicionei novas imagens ao acervo da galeria.

--- src/js/swiperScript.js
+++ src/js/swiperScript.js
@@ -2,6 +2,10 @@ const swiper = new Swiper(".swiper", {
   pagination: {
     el: ".swiper-pagination",
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   speed: 400,
   spaceBetween: 75,
   loop: true,
