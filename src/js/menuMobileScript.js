aab774fe4fdaf0312dc93d205880815919e9a81b
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-03-12 14:54:22 -0300

    Adicionando animação
    Adicionei animação onScroll testando um recurso nativo do CSS. (não pega no fire fox e safari kkkkk)

--- src/js/menuMobileScript.js
+++ src/js/menuMobileScript.js
@@ -1,6 +1,7 @@
 const btnMenu = document.getElementById("menu-icon");
 const myMenu = document.getElementById("menu");
 const menuParent = document.getElementById("menu-flex-box");
+const menuItens = document.getElementById("menu-itens");
 
 function addExtraSpaceSwitch() {
   menuParent.classList.add("section-spacing");
@@ -13,10 +14,12 @@ function menuSwitch() {
   if (myMenu.style.opacity == "1") {
     myMenu.style.opacity = "0";
     myMenu.style.maxHeight = "0";
+    menuItens.style.display = "none";
     removeExtraSpaceSwitch();
   } else {
     myMenu.style.opacity = "1";
     myMenu.style.maxHeight = "100%";
+    menuItens.style.display = "inline-block";
     addExtraSpaceSwitch();
   }
 }
