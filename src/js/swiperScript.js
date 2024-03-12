04c7d4ab1cbf2dc2e96812910d19719329475cd2
Vini-Ferraz <132089057+Vini-Ferraz@users.noreply.github.com>
2024-03-11 22:06:42 -0300

    Torando o carrossel responsivo
    Agora o carrossel se adapta a todos os tamanhos de telas.

--- src/js/swiperScript.js
+++ src/js/swiperScript.js
@@ -1,12 +1,32 @@
 const swiper = new Swiper(".swiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  speed: 400,
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    "@1.00": {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    "@1.50": {
+      slidesPerView: 3,
+      spaceBetween: 40,
+    },
   },
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   },
-  speed: 400,
-  spaceBetween: 75,
-  loop: true,
 });
